7c15d5d0dcd33fdfeefcfca06475bec9c71906c3
AnandDev <anandrathi.dev@gmail.com>
2017-04-18 12:23:27 +0530

    Create DFTools.R

---
+++ DFTools.R
@@ -0,0 +1,8 @@
+# Transpose a data frame 
+# that is rows becomes columns and colnames get replaced by rownames 
+tDF <- function( x ) {
+  x.T <- t(x[,2:ncol(x)])
+  colnames(x.T) <- x[,1] 
+  return(as.data.frame(x.T))
+}
+
